5cadb0e1b4c5f82dd9bc57a9bb12f912179dd3ba
el jefecito <thobiloba33@gmail.com>
2023-09-13 17:32:40 +0100

    calc-commit

--- index.js
+++ index.js
@@ -1,178 +1,123 @@
-const gameBoard = document.querySelector("#gameBoard");
-const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector("#scoreText");
-const reset = document.querySelector("#resetBtn");
-const gameWidth = gameBoard.width;
-const gameHeight = gameBoard.height;
-const boardBackground = "wheat";
-const snakeColor = "lightgreen";
-const snakeBorder = "black";
-const foodColor = "red";
-const unitSize = 25;
-let running = false;
-let xVelocity = unitSize;
-let yVelocity = 0;
-let foodX;
-let foodY;
-let score = 0;
-let snake = [
-    { x: unitSize * 4, y: 0 },
-    { x: unitSize * 3, y: 0 },
-    { x: unitSize * 2, y: 0 },
-    { x: unitSize, y: 0 },
-    { x: 0, y: 0 }
-]
+const keys = document.querySelectorAll(".key");
+const display_input = document.querySelector(".display .input");
+const display_output = document.querySelector(".display .output");
 
-window.addEventListener("keydown", changeDirection);
-resetBtn.addEventListener("click", resetGame);
+let input = "";
 
-gameStart();
+for (let key of keys) {
+    const value = key.dataset.key
 
-function gameStart() {
-    running = true;
-    scoreText.textContent = score;
-    createFood();
-    drawFood();
-    nextTick();
-}
+    key.addEventListener("click", () => {
+        if (value == "clear") {
+            input = "";
+            display_input.innerHTML = "";
+            display_output.innerHTML = "";
+        }
+        else if (value == "backspace") {
+            input = input.slice(0, -1);
+            display_input.innerHTML = cleanInput(input);
+        }
+        else if (value == "=") {
+            let result = eval(prepareInput(input));
+            display_output.innerHTML = cleanOutput(result)
+        }
+        else if (value == "brackets") {
+            if (input.indexOf("(") == -1 ||
+                input.indexOf("(") != -1 &&
+                input.indexOf(")") != -1 &&
+                input.lastIndexOf("(") < input.lastIndexOf(")")
+            ) {
+                input += "(";
+            }
+            else if (
+                input.indexOf("(") != -1 &&
+                input.indexOf(")") == -1 ||
+                input.indexOf("(") != -1 &&
+                input.indexOf(")") != -1 &&
+                input.lastIndexOf("(") > input.lastIndexOf(")")
+            ) {
+                input += ")";
+            }
+            display_input.innerHTML = cleanInput(input);
+        } else {
+            if (validateInput(value)) {
+                input += value;
+                display_input.innerHTML = cleanInput(input);
+            }
 
-function nextTick() {
-    if (running) {
-        setTimeout(() => {
-            clearBoard();
-            drawFood();
-            moveSnake();
-            drawSnake();
-            checkGameOver();
-            nextTick();
-        }, 75);
-    }
-    else {
-        displayGameOver();
-    }
-}
-
-function clearBoard() {
-    ctx.fillStyle = boardBackground;
-    ctx.fillRect(0, 0, gameWidth, gameHeight);
-}
-
-function createFood() {
-    function randomFood(min, max) {
-        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
-        return randNum;
-    }
-    foodX = randomFood(0, gameWidth - unitSize);
-    foodY = randomFood(0, gameWidth - unitSize);
-}
-function drawFood() {
-    ctx.fillStyle = foodColor;
-    ctx.fillRect(foodX, foodY, unitSize, unitSize);
-}
-
-function moveSnake() {
-    const head = {
-        x: snake[0].x + xVelocity,
-        y: snake[0].y + yVelocity
-    };
-    snake.unshift(head);
-    // if food is eaten
-    if (snake[0].x == foodX && snake[0].y == foodY) {
-        score += 1;
-        scoreText.textContent = score;
-        createFood();
-    }
-    else {
-        snake.pop();
-    }
-};
-
-function drawSnake() {
-    ctx.fillStyle = snakeColor;
-    ctx.strokeStyle = snakeBorder;
-    snake.forEach(snakePart => {
-        ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
-        ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+        }
     })
 }
 
-function changeDirection(event) {
-    const keyPressed = event.keyCode;
-    const LEFT = 37;
-    const RIGHT = 39;
-    const UP = 38;
-    const DOWN = 40;
+function cleanInput(input) {
+    let input_array = input.split("");
+    let input_array_lenght = input_array.length;
 
-    const goingUp = (yVelocity == -unitSize);
-    const goingDown = (yVelocity == unitSize);
-    const goingRight = (xVelocity == unitSize);
-    const goingLeft = (xVelocity == -unitSize);
+    for (let i = 0; i < input_array_lenght; i++) {
+        if (input_array[i] == "*") {
+            input_array[i] = ` <span class="operator">x</span> `
+        } else if (input_array[i] == "/") {
+            input_array[i] = ` <span class="operator">÷</span> `
+        } else if (input_array[i] == "+") {
+            input_array[i] = ` <span class="operator">+</span> `
+        } else if (input_array[i] == "-") {
+            input_array[i] = ` <span class="operator">-</span> `
+        } else if (input_array[i] == "(") {
+            input_array[i] = `<span class="brackets">(</span>`
+        } else if (input_array[i] == ")") {
+            input_array[i] = `<span class="brackets">)</span>`
+        } else if (input_array[i] == "%") {
+            input_array[i] = ` <span class="percent">%</span> `
+        }
 
-    switch (true) {
-        case (keyPressed == LEFT && !goingRight):
-            xVelocity = -unitSize;
-            yVelocity = 0;
-            break;
-        case (keyPressed == UP && !goingDown):
-            xVelocity = 0;
-            yVelocity = -unitSize;
-            break;
-        case (keyPressed == RIGHT && !goingLeft):
-            xVelocity = unitSize;
-            yVelocity = 0;
-            break;
-        case (keyPressed == DOWN && !goingUp):
-            xVelocity = 0;
-            yVelocity = unitSize;
-            break;
     }
+    return input_array.join("");
 }
 
-function checkGameOver() {
-    switch (true) {
-        case (snake[0].x < 0):
-            running = false;
-            break;
+function cleanOutput(output) {
+    let output_string = output.toString();
+    let decimal = output_string.split(".")[1];
+    output_string = output_string.split(".")[0];
+
+    let output_array = output_string.split("");
+    if (output_array.length > 3) {
+        for (let i = output_array.length - 3; i > 0; i -= 3) {
+            output_array.splice(i, 0, ",");
+        }
     }
-    switch (true) {
-        case (snake[0].x >= gameWidth):
-            running = false;
-            break;
+
+    if (decimal) {
+        output_array.push(".");
+        output_array.push(decimal);
     }
-    switch (true) {
-        case (snake[0].y < 0):
-            running = false;
-            break;
-    }
-    switch (true) {
-        case (snake[0].y >= gameHeight):
-            running = false;
-            break;
-    }
-    for (let i = 1; i < snake.length; i += 1) {
-        if (snake[i].x == snake[0].x && snake[i].y == snake[0].y)
-            running = false;
-    }
+    return output_array.join("");
 }
 
-function displayGameOver() {
-    ctx.font = "50px MV Boli";
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2);
-    running = false;
+function validateInput(value) {
+    let last_input = input.slice(-1);
+    let operators = ["+", "-", "*", "/"];
+
+    if (value == "." && last_input == ".") {
+        return false;
+    }
+
+    if (operators.includes(value)) {
+        if (operators.includes(last_input)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    return true;
 }
 
-function resetGame() {
-    score = 0;
-    xVelocity = unitSize;
-    yVelocity = 0;
-    snake = [
-        { x: unitSize * 4, y: 0 },
-        { x: unitSize * 3, y: 0 },
-        { x: unitSize * 2, y: 0 },
-        { x: unitSize, y: 0 },
-        { x: 0, y: 0 }
-    ];
-    gameStart()
+function prepareInput(input) {
+    let input_array = input.split("");
+
+    for (let i = 0; i < input_array.length; i++) {
+        if (input_array[i] == "%") {
+            input_array[i] = "/100";
+        }
+    }
+    return input_array.join("");
 }
